5ad4a9485e686646c96d3fd8f0680bd12e0ce79f
Sander Nales <sander.nales@vinkinstallatiegroep.nl>
2025-07-04 09:36:20 +0200

    maven structure added

---
+++ src/test/java/nl/vink/func_omschr/FunctioneleOmschrijvingApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.vink.func_omschr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctioneleOmschrijvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
